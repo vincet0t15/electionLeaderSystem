ddebecd1a33d865e1c80c85a22e59d74f9a67284
vincet0t15 <vincet0t15@gmail.com>
2024-08-20 22:19:49 +0800

    updated

--- resources/js/components/Masterpage/SideBar.jsx
+++ resources/js/components/Masterpage/SideBar.jsx
@@ -1,93 +1,127 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+    HomeModernIcon,
+    HomeIcon,
+    UsersIcon,
+    BuildingOfficeIcon,
+    IdentificationIcon,
+    UserPlusIcon,
+    MapPinIcon,
+    FingerPrintIcon,
+    ArrowDownOnSquareStackIcon,
+} from "@heroicons/react/24/outline";
+import { NavLink } from "react-router-dom";
 
 export default function SideBar() {
+    const [activeSidebar, setActiveSidebar] = useState(() => {
+        return localStorage.getItem("active-sidebar");
+    });
+
+    const ClickActiveSideBar = (value) => {
+        setActiveSidebar(value);
+        localStorage.setItem("active-sidebar", value);
+    };
+    const [pages, setPages] = useState([
+        {
+            title: "General",
+            children: [
+                {
+                    title: "Dashboard",
+                    to: "dashboard",
+                    icon: HomeIcon,
+                    isLast: 0,
+                },
+                {
+                    title: "Downline",
+                    to: "downline",
+                    icon: ArrowDownOnSquareStackIcon,
+                    isLast: 0,
+                },
+                {
+                    title: "Leaders",
+                    to: "leaders",
+                    icon: ArrowDownOnSquareStackIcon,
+                    isLast: 0,
+                },
+            ],
+        },
+
+        {
+            title: "Settings",
+            children: [
+                {
+                    title: "Barangay",
+                    to: "settings-barangay",
+                    icon: MapPinIcon,
+                    isLast: 0,
+                },
+                {
+                    title: "Profile",
+                    to: "settings-profile-list",
+                    icon: UserPlusIcon,
+                    isLast: 0,
+                },
+                {
+                    title: "Precinct",
+                    to: "settings-precint",
+                    icon: BuildingOfficeIcon,
+                    isLast: 0,
+                },
+                {
+                    title: "Position",
+                    to: "settings-position",
+                    icon: IdentificationIcon,
+                    isLast: 0,
+                },
+                {
+                    title: "User",
+                    to: "settings-users",
+                    icon: UsersIcon,
+                    isLast: 0,
+                },
+            ],
+        },
+    ]);
+
+    useEffect(() => {
+        const active = localStorage.getItem("active-sidebar");
+        active
+            ? localStorage.setItem("active-sidebar", active)
+            : localStorage.setItem("active-sidebar", "dashboard");
+        setActiveSidebar(active);
+    }, [activeSidebar]);
+
     return (
         <div className="mt-14 h-screen w-full">
             <aside className="scrollbar-hide h-full w-full p-6 sm:w-60 dark:bg-gray-50 dark:text-gray-800 drop-shadow overflow-y-auto">
                 <nav className="space-y-8 text-sm">
-                    <div className="space-y-2">
-                        <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-600">
-                            Getting Started
-                        </h2>
-                        <div className="flex flex-col space-y-1">
-                            <a
-                                rel="noopener noreferrer"
-                                href="#"
-                                className="hover:font-bold hover:text-teal-600"
-                            >
-                                Installation
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Plugins
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Migrations
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Appearance
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Mamba UI
-                            </a>
+                    {pages.map((item, index) => (
+                        <div className="space-y-2" key={index}>
+                            <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-600">
+                                {item.title}
+                            </h2>
+                            <div className="flex flex-col space-y-1">
+                                {item.children.map((item, index) => (
+                                    <NavLink
+                                        to={item.to}
+                                        onClick={() =>
+                                            ClickActiveSideBar(item.to)
+                                        }
+                                        key={index}
+                                        rel="noopener noreferrer"
+                                        href="#"
+                                        className={`${
+                                            activeSidebar === item.to
+                                                ? "text-teal-600 font-bold"
+                                                : "hover:font-bold hover:text-teal-600"
+                                        } `}
+                                    >
+                                        {item.title}
+                                    </NavLink>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                    <div className="space-y-2">
-                        <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-600">
-                            Dashboard
-                        </h2>
-                        <div className="flex flex-col space-y-1">
-                            <a rel="noopener noreferrer" href="#">
-                                Header
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Drawer
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Page Title
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Menus
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Sidebar
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Footer
-                            </a>
-                        </div>
-                    </div>
-                    <div className="space-y-2">
-                        <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-600">
-                            Pages
-                        </h2>
-                        <div className="flex flex-col space-y-1">
-                            <a rel="noopener noreferrer" href="#">
-                                Homepage
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Users
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Tools
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Settings
-                            </a>
-                        </div>
-                    </div>
-                    <div className="space-y-2">
-                        <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-600">
-                            Misc
-                        </h2>
-                        <div className="flex flex-col space-y-1">
-                            <a rel="noopener noreferrer" href="#">
-                                Tutorials
-                            </a>
-                            <a rel="noopener noreferrer" href="#">
-                                Changelog
-                            </a>
-                        </div>
-                    </div>
+                    ))}
                 </nav>
             </aside>
         </div>
