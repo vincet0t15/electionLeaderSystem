a8da87baad3f82cdbb11c8a11a19d38ac58d9abf
vincet0t15 <vincet0t15@gmail.com>
2024-08-21 11:34:02 +0800

    updated

--- app/Models/Barangay.php
+++ app/Models/Barangay.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use Carbon\Carbon;
-use Illuminate\Container\Attributes\Auth;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
 
 class Barangay extends Model
 {
@@ -21,7 +21,6 @@ class Barangay extends Model
     public static function boot()
     {
         parent::boot();
-
 
         static::creating(function ($model) {
             $model->date_created = $model->date_created ?? Carbon::now('singapore')->toDateTimeString();
